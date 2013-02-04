218a396ff4a23ecf85aa2e3c7b7fc9a4c0925c42
steve <steve@tuvok>
2013-02-04 16:10:31 -0600

    LOL. office power failure. commit & sync before UPS dies

--- trunk/core/JPlayerSWF.js
+++ trunk/core/JPlayerSWF.js
@@ -11,13 +11,64 @@ Ext.define('Ext.ux.JPlayerSWF',{
 	alias: ['widget.jplayerswf'],
 	requires: ['Ext.flash.Component'],
 	url: 'Jplayer.swf',
-	constructor: function(config){
-		// <debug>
-			console.log('Inside Ext.ux.JPlayerSWF constructor');
-			console.log('config', config);
-		// </debug>
-		return this.callParent(config);	
-	}
+        media : {},
+        wmode: 'window',
+        flashParams: {
+            allowScriptAccess: 'always'
+        }, 
+	initComponent: function(config){ 
+            // <debug>
+                console.log('Inside Ext.ux.JPlayerSWF constructor');
+                console.log('config', config);
+            // </debug>
+
+            this.callParent(config);	
+            
+	},
+        afterRender: function(){
+            var me = this,
+                flashParams = Ext.apply({}, me.flashParams), 
+                flashVars = Ext.apply({}, me.flashVars);
+
+            me.callParent();
+
+            flashParams = Ext.apply({
+                allowScriptAccess: 'always',
+                bgcolor: me.backgroundColor,
+                wmode: me.wmode
+            }, flashParams);
+
+            flashVars = Ext.apply({
+                allowedDomain: document.location.hostname
+            }, flashVars);
+
+            new swfobject.embedSWF(
+                me.url,
+                me.getSwfId(),
+                me.swfWidth,
+                me.swfHeight,
+                me.flashVersion,
+                me.expressInstall ? me.statics.EXPRESS_INSTALL_URL : undefined,
+                flashVars,
+                flashParams,
+                me.flashAttributes,
+                Ext.bind(me.swfCallback, me)
+            );            
+        },
+        listeners: {
+            afterRender: function(t){
+                console.log('afterRender t', t);
+                console.log(this);
+                console.log('this.swf',this.swf);
+                console.log(document.getElementById(this.swfId));
+                console.log('swfobject.getObjectById(this.swfId)',swfobject.getObjectById(this.swfId));
+                var test = swfobject.getObjectById(this.swfId);
+                    //test.trigger();
+               // document.getElementById(this.swfId).setFile("http://www.jplayer.org/audio/m4a/TSP-01-Cro_magnon_man.m4a");
+                //this.swf.setFile();
+                //this.swf.play();
+            }
+        }
 }, function(){
 	Ext.apply(this.prototype, {
 		config: {
--- trunk/ux/jplayer.js
+++ trunk/ux/jplayer.js
@@ -1,23 +1,23 @@
 Ext.define('Ext.ux.jplayer', {
-	extend: 'Ext.Component',
-	alias: ['widget.jplayer'],
-	requires: ['Ext.ux.JPlayerSWF'],
-	statics: {
-		instance: Ext.ux.JPlayerSWF
-	},
-	constructor: function(config){
-		console.log('inside ExtJplayer')
+    extend: 'Ext.Component',
+    alias: ['widget.jplayer'],
+    requires: ['Ext.ux.JPlayerSWF'],
+    statics: {
+            instance: Ext.ux.JPlayerSWF
+    },
+    constructor: function(config){
+        console.log('inside ExtJplayer')
         this.initConfig(config);
         console.log('config->', config);
 
         console.log('Ext.supports',Ext.supports);
         if(Ext.supports.AudioTag){
-        	this.requireFlash = false;
+                this.requireFlash = false;
         }
         console.log('this in jplayer',this);
         return this.callParent(config);
-	},
-	pause: function() {
-		// 		element.jPlayer("pause");
-	}
+    },
+    pause: function() {
+            // 		element.jPlayer("pause");
+    }
 });
